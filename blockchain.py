3b1b5dcb010e5cd4c0f9a0a21dc5acb31789bf51
e-r-i-k-a <samuels.erika@yahoo.com>
2018-07-06 23:57:50 -0400

    implemented consensus algo

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import hashlib
 import json
+import requests
 from time import time
 from uuid import uuid4
 from textwrap import dedent
@@ -12,6 +13,12 @@ class Blockchain(object):
     self.chain = []
     self.current_transactions = []
     self.new_block(previous_hash=1, proof=100)
+    self.nodes = set()
+
+  def register_node(self, address):
+    #add a new node to the list of nodes
+    parsed_url = urlparse(address)
+    self.nodes.add(parsed_url.netloc)
 
   def new_block(self, proof, previous_hash=None):
     # create a new Block, add it to the chain, and return it
@@ -37,6 +44,47 @@ class Blockchain(object):
     })
     return self.last_block['index'] + 1
 
+  def valid_chain(self, chain):
+    # determine if a given blockchain is valid by looping through each block and verifying both the hash and proof
+    last_block = chain[0]
+    current_index = 1
+    while current_index < len(chain):
+      block = chain[current_index]
+      print(f'{last_block}')
+      print(f'{block}')
+      print("\n-------------\n")
+      # validate hash
+      if block['previous_hash'] != self.hash(last_block):
+        return False
+      # validate proof
+      if not self.valid_proof(last_block['proof'], block['proof']):
+        return False
+      last_block = block
+      current_index += 1
+    return True
+
+    def resolve_conflicts(self):
+      # Consensus algorithm that loops through all of our neighboring nodes, downloads their chains, and verifies them using the 'valid_chain()' method.  if a longer, valid chain is found, we replace ours
+      neighbors = self.nodes
+      new_chain = None
+      max_length = len(self.chain)
+      # grab and verify the chains from all the nodes in our netowrk
+      for node in neighbors:
+        response = requests.get(f'http://{node}/chain')
+        if response.status_code == 200:
+          length = response.json()['length']
+          chain = response.json()['chain']
+          # check if the length is longer and the chain is valid
+          if length > max_length and self.valid_chain(chain):
+            max_length = length
+            new_chain = chain
+      # replace our chain if we discovered a new, longer valid chain
+      if new_chain:
+        self.chain = new_chain
+        return True
+
+      return False
+
   @staticmethod
   def hash(block):
     # create a SHA-256 hash of a Block
@@ -60,11 +108,9 @@ class Blockchain(object):
   @staticmethod
   def valid_proof(last_proof, proof):
     #return x where hashlib.sha256(x) last 4 digits = 0000
-    guess = f'{last_proof}{proof}'.endcode()
+    guess = f'{last_proof}{proof}'.encode()
     guess_hash = hashlib.sha256(guess).hexdigest()
 
-
-####################################################
 
 # node init
 app = Flask(__name__)
@@ -72,6 +118,9 @@ app = Flask(__name__)
 node_identifier = str(uuid4()).replace('-','')
 # blockchain init
 blockchain = Blockchain()
+
+########
+# routes
 
 @app.route('/mine', methods=['GET'])
 def mine():
@@ -117,6 +166,38 @@ def full_chain():
     'length': len(blockchain.chain)
   }
   return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+  values = request.get_json()
+  nodes = values.get('nodes')
+  if nodes is None:
+    return "Error: Please supply a valid list of nodes", 400
+  for node in nodes:
+    blockchain.register_node(node)
+  response = {
+    'message': 'New nodes have been added',
+    'total_nodes': list(blockchain.nodes)
+  }
+  return jsonify(response), 201
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+  replaced = blockchain.resolve_conflicts()
+  if replaced:
+    response = {
+      'message': 'Our chain was replaced',
+      'new_chain': blockchain.chain
+    }
+  else:
+    response = {
+      'message': 'Our chain is authoritative',
+      'chain': blockchain.chain
+    }
+  return jsonify(response), 200
+
+##################
+# start the server
 
 if __name__ == '__main__':
   # run the server
